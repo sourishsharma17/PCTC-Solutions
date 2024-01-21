70f045813fca44f55a0c87b9d0e5a3c4f63055fc
Sourish <70473280+SourishS17@users.noreply.github.com>
2024-01-21 13:13:35 +0000

    Create q7.py

---
+++ 2020-23-R1/q7.py
@@ -0,0 +1,39 @@
+num = int(input())
+names = [input() for i in range(num)]
+
+while 1:
+  ended = 0
+
+  names.sort()
+
+  eliminator = names.pop(0)
+
+  if len(names) == 1:
+    print(names[0])
+    break
+  if len(names) == 0:
+    print("NO RESULT")
+    break
+
+  for letter in eliminator:
+
+    to_eliminate = []
+
+    for name in names:
+      if letter in name:
+        to_eliminate.append(name)
+
+    for name in to_eliminate:
+      names.remove(name)
+
+    if len(names) == 1:
+      print(names[0])
+      ended = 1
+      break
+    if len(names) == 0:
+      print("NO RESULT")
+      ended = 1
+      break
+
+  if ended == 1:
+    break
